f5fbe7306cce8e7e1303a49592dfaab561d7333e
TheThinh1506 <23521506@gm.uit.edu.vn>
2026-06-06 16:23:28 +0700

    lab02 part1: fix terraform format

--- Lab02/part1-terraform/modules/vpc/main.tf
+++ Lab02/part1-terraform/modules/vpc/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = var.vpc_name }
+  tags                 = { Name = var.vpc_name }
 }
 
 resource "aws_subnet" "public" {
@@ -10,14 +10,14 @@ resource "aws_subnet" "public" {
   cidr_block              = var.public_subnet_cidr
   availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
-  tags = { Name = "${var.vpc_name}-public" }
+  tags                    = { Name = "${var.vpc_name}-public" }
 }
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = var.private_subnet_cidr
   availability_zone = var.availability_zone
-  tags = { Name = "${var.vpc_name}-private" }
+  tags              = { Name = "${var.vpc_name}-private" }
 }
 
 resource "aws_internet_gateway" "this" {
